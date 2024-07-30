42f579c14e817a4925b2f629812962ee3c5f3bf1
Victor Koech <vickoch20@gmail.com>
2024-07-30 09:29:06 +0300

    Add index.js file as Node.js server

---
+++ index.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  let filePath = '.' + req.url;
+  if (filePath === './') {
+    filePath = './index.html';
+  } else if (filePath === './about') {
+    filePath = './about.html';
+  } else if (filePath === './contact-me') {
+    filePath = './contact-me.html';
+  }
+
+  const extname = String(path.extname(filePath)).toLowerCase();
+  const contentType = 'text/html';
+
+  fs.readFile(filePath, (error, content) => {
+    if (error) {
+      if (error.code === 'ENOENT') {
+        fs.readFile('./404.html', (error, content) => {
+          res.writeHead(404, { 'Content-Type': contentType });
+          res.end(content, 'utf-8');
+        });
+      } else {
+        res.writeHead(500);
+        res.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+const port = 8080;
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
